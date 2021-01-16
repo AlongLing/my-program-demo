d6e089bcdd1352ac0733f5d798404b033fdc1d53
xiongalong <13184578175@163.com>
2021-01-16 10:13:20 +0800

    新增 nodejs http 请求 demo

---
+++ nodejs-demo/http-test/app.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const querystring = require('querystring')
+
+const server = http.createServer((req, res) => {
+
+})
+
+server.listen(8000, () => {
+  console.log('listen on 8000 port')
+})
